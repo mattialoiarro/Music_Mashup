f0b5a97252897d418f2cab86d78ddfc06442ef48
guywithoutlif3 <72743368+guywithoutlif3@users.noreply.github.com>
2022-12-20 13:08:44 +0100

    added search, login and logout

--- music-mashup/src/App.js
+++ music-mashup/src/App.js
@@ -1,34 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from 'react-bootstrap/Nav';
-import { useState, useEffect } from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "react-bootstrap/Nav";
+import { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { If, Then, Else, When, Unless, Switch, Case, Default } from "react-if";
 
 function App() {
   const [inputSearch, setInputSearch] = useState("");
   const [playlistresult, setPlaylistresult] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlistURL, setPlaylistURL] = useState("");
 
-  function getPlaylist() {
+  const [token, setToken] = useState("");
+
+  const CLIENT_ID = "452798eb3b5a4449845f7bc544647e10";
+  const REDIRECT_URI = "http://localhost:3000/callback";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+  function getPlaylist(id) {
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer BQADCrDD62cBTa0FfjU-uDHvxOJtVBo-DcNjxszfmE3euf4Usc8HJX4tYFKHeGUdB4egr7oZ6DryNz0BaJoXKiL5G2Gyq5uM0Hx55OoIsgI9p4-1RDp6qE4wdhPTbzjGFEiJzO10ySrUCtDUEM3qLX7l4T9aYR4ap1q7qAmESJJ2DRhokPhxqd2KOSBauxAGG2ZpWBsf7S3eEtpq00iRQdlRYUM-bgOYBXDiaw");
-    
+    myHeaders.append("Authorization", "Bearer " + token);
+
     var requestOptions = {
-      method: 'GET',
+      method: "GET",
       headers: myHeaders,
-      redirect: 'follow'
+      redirect: "follow",
     };
 
-    fetch("https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n/tracks", requestOptions)
-      .then(response => response.json())
-      .then(result => setPlaylistresult(result.items))
-      .catch(error => console.log('error', error));
+    fetch(
+      "https://api.spotify.com/v1/playlists/" + id + "/tracks",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setPlaylistresult(result.items))
+      .catch((error) => console.log("error", error));
   }
 
-  const listItems = playlistresult.map((item) =>
+  function searchPlaylist() {}
 
-    <Card class="songCard" style={{ width: '18rem', border: '3px solid #191414' }}>
-      <Card.Img variant="top" src={item.track.album.images[0].url} height="40%" width="100%" />
+  const listItems = playlistresult.map((item) => (
+    <Card
+      class="songCard"
+      style={{ width: "18rem", border: "3px solid #191414" }}
+    >
+      <Card.Img
+        variant="top"
+        src={item.track.album.images[0].url}
+        height="40%"
+        width="100%"
+      />
       <Card.Body>
         <Card.Title>{item.track.name}</Card.Title>
         <Card.Text>
@@ -38,36 +84,96 @@ function App() {
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
-  );
-
+  ));
 
   return (
     <div className="App">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
 
       <div class="navbar">
         <a href="#home"> Music Mashup</a>
         <link rel="stylesheet" href="css/font-awesome.css" />
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
-
+        <link
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+          rel="stylesheet"
+        />
       </div>
+      <header className="App-header">
+        <h1>Spotify React</h1>
+        {!token ? (
+          <a
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+          >
+            Login to Spotify
+          </a>
+        ) : (
+          <div>
+            {" "}
+            <div class="main">
+              <p>Search for your favorite playlist and find out more</p>
 
-      <div class="main">
-        <p>Search for your favorite playlist and find out more</p>
-        <div class="box">
-          <form name="search">
-            <input onChange={(e) => setInputSearch(e.target.value)} type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();">
-
-            </input>
-
-
-          </form>
-          <button onClick={getPlaylist}></button>
-        </div>
-        <div class="card mx-auto" style={{width: "18rem;"}}>
-          {listItems}
-        </div>
-      </div>
+              <div class="box">
+                <form name="search">
+                  <input
+                    onChange={(e) => {
+                      //TODO: make search not be alwys one behind
+                      setInputSearch(e.target.value);
+                      //-------------------------------------------
+                      var myHeaders = new Headers();
+                      myHeaders.append("Authorization", "Bearer " + token);
+                      var requestOptions = {
+                        method: "GET",
+                        headers: myHeaders,
+                        redirect: "follow",
+                      };
+                      if (inputSearch == "") {
+                        
+                      } else {
+                        fetch(
+                          "https://api.spotify.com/v1/search?query=" +
+                            inputSearch +
+                            "&type=playlist&limit=3&offset=0",
+                          requestOptions
+                        )
+                          .then((response) => response.json())
+                          .then((result) =>
+                            setSearchResults(result.playlists.items)
+                          )
+                          .catch((error) => console.log("error", error));
+                      }
+                    }}
+                    type="text"
+                    class="input"
+                    name="txt"
+                    onmouseout="this.value = ''; this.blur();"
+                  ></input>
+                </form>
+                {listItems}
+                <button onClick={getPlaylist}></button>
+              </div>
+              <div class="card mx-auto" style={{ width: "18rem;" }}>
+                <If condition={searchResults === []}>
+                  <Then></Then>
+                </If>
+                <Else>
+                  {Array.isArray(searchResults)
+                    ? searchResults.map((item) => {
+                        return <p onClick={event => {
+                          setPlaylistURL(item.id);
+                          getPlaylist(item.id);
+                        }}>{item.name}</p>;
+                      })
+                    : null}
+                </Else>
+              </div>
+            </div>{" "}
+            <button onClick={logout}>Logout</button>
+          </div>
+        )}
+      </header>
     </div>
   );
 }
