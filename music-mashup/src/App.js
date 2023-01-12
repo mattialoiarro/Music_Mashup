814427543491474e384d9037fc6a5460d8d03c0f
guywithoutlif3 <72743368+guywithoutlif3@users.noreply.github.com>
2023-01-12 13:41:16 +0100

    styling

--- music-mashup/src/App.js
+++ music-mashup/src/App.js
@@ -75,8 +75,9 @@ function App() {
   const listItems = playlistresult.map((item) => (
     <Card
       class="songCard"
-      style={{ width: "18rem", border: "3px solid #191414", padding: ".5rem", backgroundColor: "#191414",filter: "drop-shadow(-5px 3.8px 3.8px #000)"
-    }}
+      style={{
+        width: "18rem", border: "3px solid #191414", padding: ".5rem", backgroundColor: "#191414", filter: "drop-shadow(-5px 3.8px 3.8px #000)"
+      }}
     >
       <Card.Img
         variant="top"
@@ -94,7 +95,7 @@ function App() {
             </div>
           ))}
         </Card.Text>
-        <Button class="getLyrics" style={{
+        <Button class="mt-auto btn btn-lg btn-block btn-outline-primary" style={{
           border: " 2px solid #1DB954",
           backgroundColor: "white",
           borderRadius: "20px",
@@ -103,8 +104,13 @@ function App() {
           paddingTop: "0.3em",
           paddingBottom: "0.3em",
           fontSize: "1em",
+          position: "absolute",
+          left: "25%",
+          right: "25%",
+          bottom: "0",
           color: "#1DB954",
-          filter: "drop-shadow(-1px 1.7px 1.7px white)"
+          filter: "drop-shadow(-1px 1.7px 1.7px white)",
+          marginBottom: "3%"
         }} onClick={() => {
           const url =
             "https://spotify-scraper.p.rapidapi.com/v1/track/lyrics?trackId=" + item.track.id;
@@ -126,7 +132,14 @@ function App() {
 
 
         }}>Get Lyrics</Button>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16">
+          <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" stroke="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
+          <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" />
+        </svg>
       </Card.Body>
+
     </Card>
   ));
 
