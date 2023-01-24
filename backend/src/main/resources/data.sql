615ac897775c7dddcd68252d32bed2f217acfac7
GianIuca <gianluca.ferrara@ti8m.ch>
2023-01-24 09:17:08 +0100

    deleted backend

--- backend/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO song (spotifyTrackID, title, artist) VALUES (1, 'song1', 'artist1');
-INSERT INTO song (spotifyTrackID, title, artist) VALUES (2, 'song2', 'artist2');
-INSERT INTO song (spotifyTrackID, title, artist) VALUES (3, 'song3', 'artist3');
--- backend/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE if EXISTS song;
-
-CREATE TABLE song
-(
-    id             int IDENTITY(1,1) PRIMARY KEY,
-    spotifyTrackID INT,
-    title          VARCHAR(255),
-    artist         VARCHAR(255)
-);
